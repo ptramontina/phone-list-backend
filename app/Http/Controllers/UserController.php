f7108a4600488d4e07ae69e0a67a5ec958b9325a
Pedro Ernesto Tramontina <pedro.tramontina@gmail.com>
2020-03-18 22:32:49 -0300

    Add hash to store and update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -29,6 +30,8 @@ class UserController extends Controller
     public function store(Request $request)
     {
         try {
+            $request['password'] = Hash::make($request['password']);
+
             $user = User::create($request->all());
 
             return response()->json([
@@ -64,7 +67,13 @@ class UserController extends Controller
     public function update(Request $request, User $user)
     {
         try {
-            $user->update(!$request->password ? $request->except('password') : $request->all());
+            $user->update($request->except('password'));
+            
+            if ($request->password != null) {
+                $password = Hash::make($request->password);
+                $user->password = $password;
+                $user->save();
+            } 
 
             return response()->json([
                 'success' => true,
